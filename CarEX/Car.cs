8caa6b39adab73c06d8bae67f76d39f98bf90126
BlakeStanley5 <blakestanley542@gmail.com>
2020-11-02 15:51:44 -0600

    finished

---
+++ CarEX/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarEX
+{
+    public class Car
+    {
+        public String Make { get; set; }
+        public String Model { get; set; }
+        public int Year { get; set; }
+
+    }
+
+
+
+
+}
---
+++ CarEX/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data.Common;
+
+namespace CarEX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCar = new Car();
+            myCar.Make = "Honda";
+            myCar.Model = "Accord";
+            myCar.Year = 2020;
+
+            var myCar2 = new Car()
+            {
+                Make = "Mazda",
+                Model = "CX-5",
+                Year = 2016
+            };
+
+            Console.WriteLine($"Your cars info: Make:{myCar.Make} Model:{myCar.Model}, Year:{myCar.Year}.");
+
+        }
+
+    }
+}
